43ccf1e93370d25de084259f470a684479b1e7e7
Danas <danas.mikelionis@gmail.com>
2019-04-29 12:26:40 +0300

    added(almost empty) controller class.

---
+++ src/main/java/com/vtvpmc/DanasMikelionis/trainInfo/Controller/TrainInfoController.java
@@ -0,0 +1,10 @@
+package com.vtvpmc.DanasMikelionis.trainInfo.Controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class TrainInfoController {
+	
+}

1fea5e864769b8f4552d9b0d92d2bb067d8c2221
Danas <danas.mikelionis@gmail.com>
2019-04-29 11:30:11 +0300

    Modified: application.properties. New files: WagonCargo.java, WagonLocomotive.java, WagonWithPeople.java, probably new package: ...is.model. Added 3 tables to database, WAGON_CARGO table maybe with all columns, 2 other tables probably with not all columns

---
+++ src/main/java/com/vtvpmc/DanasMikelionis/model/WagonCargo.java
@@ -0,0 +1,73 @@
+package com.vtvpmc.DanasMikelionis.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+@Entity
+public class WagonCargo {
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	private String creator;
+	private int quantityOfSameInTrain;
+	private BigDecimal price;
+	private double volumeM3;
+	private double maxLiftWeightInTones;
+	
+	public WagonCargo() { }
+
+	public Long getId() {
+		return id;
+	}
+
+	public BigDecimal getTotalPrice() {
+		return this.price.multiply(new BigDecimal(quantityOfSameInTrain));
+	}
+
+	public String getCreator() {
+		return creator;
+	}
+
+	public int getQuantityOfSameInTrain() {
+		return quantityOfSameInTrain;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public double getVolumeM3() {
+		return volumeM3;
+	}
+
+	public double getMaxLiftWeightInTones() {
+		return maxLiftWeightInTones;
+	}
+
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+
+	public void setQuantityOfSameInTrain(int quantityOfSameInTrain) {
+		this.quantityOfSameInTrain = quantityOfSameInTrain;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public void setVolumeM3(double volumeM3) {
+		this.volumeM3 = volumeM3;
+	}
+
+	public void setMaxLiftWeightInTones(double maxLiftWeightInTones) {
+		this.maxLiftWeightInTones = maxLiftWeightInTones;
+	}
+	
+	
+}
---
+++ src/main/java/com/vtvpmc/DanasMikelionis/model/WagonWithPeople.java
@@ -0,0 +1,30 @@
+package com.vtvpmc.DanasMikelionis.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+@Entity
+public class WagonWithPeople {
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	private String creator;
+	private int quantityOfSameInTrain;
+	private BigDecimal price;
+	private double volumeM3;
+	
+	public WagonWithPeople() { }
+
+	public Long getId() {
+		return id;
+	}
+	
+	public BigDecimal getTotalPrice() {
+		return this.price.multiply(new BigDecimal(quantityOfSameInTrain));
+	}
+}
